6f02d6da5a3d7b37a047bf7e39df94bb677a066b
ViktorEngman <Viktor.trelleborg@gmail.com>
2024-11-11 21:24:20 +0100

    Upp till Git

---
+++ Data/Add RecipieProducts data.sql
@@ -0,0 +1,12 @@
+--Add recipiesproducts data
+
+INSERT INTO RecipeProducts (RecipeId, ProductId, Quantity)
+VALUES
+(1, 1, 0.5),
+(1, 2, 1.0), 
+(1, 3, 0.25),
+(1, 4, 2.0),
+(2, 1, 1.5),
+(2, 2, 1.2), 
+(2, 3, 0.35),
+(2, 4, 1.8);
